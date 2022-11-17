724cb1f7416d8933512e818ca027be4bf11e3a74
Hans Rauer <h.w.rauer@googlemail.com>
2022-11-17 12:08:10 +0000

    setup env in before suite

--- keto/opl/opl_suite_test.go
+++ keto/opl/opl_suite_test.go
@@ -1,7 +1,11 @@
 package opl_test
 
 import (
+	"log"
+	"os"
 	"testing"
+
+	"github.com/joho/godotenv"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -11,3 +15,18 @@ func TestOpl(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Opl Suite")
 }
+
+var _ = BeforeSuite(func() {
+	err := godotenv.Load("../.env") // 👈 load .env file
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	Expect(os.Getenv("KETO_READ_REMOTE")).NotTo(BeZero(), "Please make sure KETO_READ_REMOTE is set correctly.")
+	Expect(os.Getenv("KETO_WRITE_REMOTE")).NotTo(BeZero(), "Please make sure KETO_WRITE_REMOTE is set correctly.")
+	Expect(os.Getenv("KETO_BEARER_TOKEN")).NotTo(BeZero(), "Please make sure KETO_WRITE_REMOTE is set correctly.")
+
+	GinkgoWriter.Printf("KETO_READ_REMOTE: %s\n", os.Getenv("KETO_READ_REMOTE"))
+	GinkgoWriter.Printf("KETO_WRITE_REMOTE: %s\n", os.Getenv("KETO_WRITE_REMOTE"))
+
+})
