4db8c53585bcead51303bae997a542921a5e0de3
Hans Rauer <h.w.rauer@googlemail.com>
2022-11-18 12:18:00 +0000

    test functionality

--- keto/opl/opl_test.go
+++ keto/opl/opl_test.go
@@ -5,69 +5,88 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	//. "github.com/onsi/gomega"
-	"github.com/ory/x/pointerx"
 
 	rts "github.com/ory/keto/proto/ory/keto/relation_tuples/v1alpha2"
 )
 
 var _ = Describe("Verify expected behaviour of the opl configuration.", func() {
 	var _ = Describe("Scenario to cover most constellations.", func() {
-		var _ = BeforeEach(func() {
-			wcl.DeleteRelationTuples(context.TODO(), &rts.DeleteRelationTuplesRequest{})
-			tuples := []*rts.RelationTuple{
-				{
-					Namespace: "Group",
-					Object:    "ops",
-					Relation:  "member",
-					Subject: rts.NewSubjectSet(
-						"User",
-						"Hans",
-						"",
-					),
-				}, {
-					Namespace: "Group",
-					Object:    "ops",
-					Relation:  "member",
-					Subject: rts.NewSubjectSet(
-						"User",
-						"David",
-						"",
-					),
-				}, {
-					Namespace: "Group",
-					Object:    "ops",
-					Relation:  "member",
-					Subject: rts.NewSubjectSet(
-						"User",
-						"Sophie",
-						"",
-					),
-				},
-			}
-			It("should be able to insert", func() {
-				getStringPtr := func(s string) *string {
-					return pointerx.Ptr(s)
-				}
-				query := rts.RelationQuery{
-					Namespace: getStringPtr("User"),
-					Object:    getStringPtr(""),
-					Relation:  getStringPtr(""),
-				}
-				_, err := wcl.DeleteRelationTuples(context.TODO(), &rts.DeleteRelationTuplesRequest{
-					RelationQuery: &query,
-				})
-				if err != nil {
-					panic("Encountered error: " + err.Error())
-				}
-				_ = tuples
-				//_, err = wcl.TransactRelationTuples(context.Background(), &rts.TransactRelationTuplesRequest{
-				//	RelationTupleDeltas: rts.RelationTupleToDeltas(tuples, rts.RelationTupleDelta_ACTION_INSERT),
-				//})
-				//if err != nil {
-				//	panic("Encountered error: " + err.Error())
-				//}
-
+		tuples := []*rts.RelationTuple{
+			{
+				Namespace: "Group",
+				Object:    "ops",
+				Relation:  "member",
+				Subject: rts.NewSubjectSet(
+					"User",
+					"Hans",
+					"",
+				),
+			}, {
+				Namespace: "Group",
+				Object:    "ops",
+				Relation:  "member",
+				Subject: rts.NewSubjectSet(
+					"User",
+					"David",
+					"",
+				),
+			}, {
+				Namespace: "Group",
+				Object:    "ops",
+				Relation:  "member",
+				Subject: rts.NewSubjectSet(
+					"User",
+					"Sophie",
+					"",
+				),
+			},
+		}
+		It("should be able to delete all", func() {
+			_, err := wcl.TransactRelationTuples(context.TODO(), &rts.TransactRelationTuplesRequest{
+				RelationTupleDeltas: rts.RelationTupleToDeltas(tuples, rts.RelationTupleDelta_ACTION_INSERT),
 			})
+			if err != nil {
+				panic("Encountered error: " + err.Error())
+			}
+			query := rts.RelationQuery{
+				Namespace: nil,
+				Object:    nil,
+				Relation:  nil,
+				Subject:   nil,
+			}
+			_, err = wcl.DeleteRelationTuples(context.TODO(), &rts.DeleteRelationTuplesRequest{
+				RelationQuery: &query,
+			})
+			if err != nil {
+				panic("Encountered error: " + err.Error())
+			}
+
+		})
+		It("should be able to list", func() {
+
+			//query := rts.RelationQuery{
+			//	Namespace: pointerx.Ptr("User"),
+			//}
+			//resp, err := rcl.ListRelationTuples(context.Background(), &rts.ListRelationTuplesRequest{
+			//	RelationQuery: &query,
+			//})
+			//if err != nil {
+			//	panic("Encountered error: " + err.Error())
+			//}
+
+			//relationTuples, err := rts.NewProtoCollection(resp.RelationTuples)
+			//if err != nil {
+			//	return err
+			//}
+			//cmdx.PrintTable(cmd, &responseOutput{
+			//	RelationTuples: relationTuples,
+			//	IsLastPage:     resp.NextPageToken == "",
+			//	NextPageToken:  resp.NextPageToken,
+			//})
+			//_, err := wcl.ListRelationTuples(context.Background(), &rts.ListRelationTuplesRequest{})
+			//if err != nil {
+			//	panic("Encountered error: " + err.Error())
+			//}
 		})
 	})
 })
