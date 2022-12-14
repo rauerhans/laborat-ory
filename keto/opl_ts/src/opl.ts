6ab028ecdcda69c6f7933dd6462a3db7ae2fd430
Hans Rauer <h.w.rauer@googlemail.com>
2022-12-14 14:22:09 +0100

    refactor schema

--- keto/opl_ts/src/opl.ts
+++ keto/opl_ts/src/opl.ts
@@ -36,37 +36,62 @@ class ResourcePolicy implements Namespace {
   related: {
     trust: (User | Group)[]
   }
+  permits = {
+    allow: (ctx: Context) => this.related.trust.includes(ctx.subject)
+  }
 }
 
-class S3ResourceType implements Namespace {
+class KubernetesResourceType implements Namespace {
   related: {
     create: Policy[]
-    write: Policy[]
-    read: Policy[]
+    delete: Policy[]
+    deletecollection: Policy[]
+    get: Policy[]
+    list: Policy[]
+    patch: Policy[]
+    update: Policy[]
+    watch: Policy[]
   }
 
   permits = {
     can_create: (ctx: Context) => this.related.create.traverse((p) => p.permits.allow(ctx)),
-    can_write: (ctx: Context) => this.related.write.traverse((p) => p.permits.allow(ctx)),
-    can_read: (ctx: Context) =>
-      this.related.read.traverse((p) => p.permits.allow(ctx)) ||
-      this.related.write.traverse((p) => p.permits.allow(ctx))
+    can_delete: (ctx: Context) => this.related.delete.traverse((p) => p.permits.allow(ctx)),
+    can_deletecollection: (ctx: Context) => this.related.delete.traverse((p) => p.permits.allow(ctx)),
+    can_get: (ctx: Context) => this.related.delete.traverse((p) => p.permits.allow(ctx)),
+    can_list: (ctx: Context) => this.related.delete.traverse((p) => p.permits.allow(ctx)),
+    can_patch: (ctx: Context) => this.related.delete.traverse((p) => p.permits.allow(ctx)),
+    can_update: (ctx: Context) => this.related.delete.traverse((p) => p.permits.allow(ctx)),
+    can_watch: (ctx: Context) => this.related.delete.traverse((p) => p.permits.allow(ctx)),
   }
 }
 
-class S3Resource implements Namespace {
+class KubricksResourceType implements Namespace {
   related: {
-    instance: S3ResourceType[]
-    write: ResourcePolicy[]
-    read: ResourcePolicy[]
+    apiaccess: Policy[]
+  }
+
+  permits = {
+    can_apiaccess: (ctx: Context) => this.related.apiaccess.traverse((p) => p.permits.allow(ctx)),
+  }
+}
+
+class ServiceResource implements Namespace {
+  related: {
+    owner: User[]
+    k8s_instance: KubernetesResourceType[]
+    kbrx_instance: KubricksResourceType[]
+    get: ResourcePolicy[]
+    watch: ResourcePolicy[]
+    apiaccess: (ResourcePolicy | Policy)[]
   }
   permits = {
-    can_write: (ctx: Context) =>
-      this.related.instance.traverse((i) => i.permits.can_write(ctx)) ||
-      this.related.write.traverse((rp) => rp.related.trust.includes(ctx.subject)),
-    can_read: (ctx: Context) =>
-      this.related.instance.traverse((i) => i.permits.can_read(ctx)) || 
-      this.related.read.traverse((rp) => rp.related.trust.includes(ctx.subject)) ||
-      this.related.write.traverse((rp) => rp.related.trust.includes(ctx.subject))
+    // Kubernetes rule verbs
+    can_delete: (ctx: Context) => this.related.k8s_instance.traverse((i) => i.permits.can_delete(ctx)) || this.related.owner.includes(ctx.subject),
+    can_get: (ctx: Context) => this.related.k8s_instance.traverse((i) => i.permits.can_get(ctx)) || this.related.owner.includes(ctx.subject),
+    can_patch: (ctx: Context) => this.related.k8s_instance.traverse((i) => i.permits.can_patch(ctx)) || this.related.owner.includes(ctx.subject),
+    can_update: (ctx: Context) => this.related.k8s_instance.traverse((i) => i.permits.can_update(ctx)) || this.related.owner.includes(ctx.subject),
+    can_watch: (ctx: Context) => this.related.k8s_instance.traverse((i) => i.permits.can_watch(ctx)) || this.related.owner.includes(ctx.subject),
+    // Kubricks rule verbs
+    can_apiaccess: (ctx: Context) => this.related.kbrx_instance.traverse((i) => i.permits.can_apiaccess(ctx)) || this.related.apiaccess.traverse((p) => p.permits.allow(ctx)) || this.related.owner.includes(ctx.subject),
   }
 }
